ce6c978e0cbfe6a43b877ebeade44576a82ef4ef
Derek Yang <yanghada@gmail.com>
2020-03-15 16:59:04 +0100

    Show status item after app launches

--- GMBuddy/AppDelegate.swift
+++ GMBuddy/AppDelegate.swift
@@ -11,16 +11,27 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+	private let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+	private let popover = NSPopover()
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
+		updateStatusButton()
 	}
-
+	
 	func applicationWillTerminate(_ aNotification: Notification) {
 		// Insert code here to tear down your application
 	}
-
-
+	
+	private func updateStatusButton() {
+		guard let button = statusItem.button else { return }
+		button.image = NSImage(named: "g")
+		button.image?.size = NSSize(width: 20, height: 20)
+		button.action = #selector(togglePopover)
+	}
+	
+	@objc func togglePopover() {
+		print("--> \(popover.isShown)")
+	}
 }
 
