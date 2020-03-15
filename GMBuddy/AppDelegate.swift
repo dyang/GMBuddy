147240edfaf3f174aa5dd07b7d3f9adbd8f785f9
Derek Yang <yanghada@gmail.com>
2020-03-15 14:41:19 +0100

    Add new Xcode project (again)

---
+++ GMBuddy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GMBuddy
+//
+//  Created by Derek Yang on 2020-03-15.
+//  Copyright © 2020 Derek Yang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
